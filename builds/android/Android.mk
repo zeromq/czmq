f3fa99cf79c7555037f6c4e7adfc0ab176ab7491
William Roberts <bill.c.roberts@gmail.com>
2011-08-10 14:02:58 -0400

    Started working on Android build, cannot get it to link zmq, and build script has an issue

---
+++ builds/android/Android.mk
@@ -0,0 +1,20 @@
+BASE_PATH := $(call my-dir)
+APP_PLATFORM = android-10
+
+#Info for libzmq
+include $(CLEAR_VARS)
+LOCAL_MODULE := zmq
+LOCAL_SRC_FILES := libzmq.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+# build czmq
+include $(CLEAR_VARS)
+LOCAL_PATH := $(BASE_PATH)
+LOCAL_MODULE := czmq
+LOCAL_C_INCLUDES := ../../include $(LIBZMQ)/include
+LOCAL_SRC_FILES := \
+		zclock.c zctx.c zlist.c zsockopt.c zsocket.c zmsg.c \
+		zloop.c zthread.c zstr.c zframe.c zhash.c
+LOCAL_SHARED_LIBRARIES := zmq
+include $(BUILD_SHARED_LIBRARY)
---
+++ builds/android/build.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+#
+#   Does an NDK build by creating the necessary JNI directory
+#   and copying the libzmq source files into a temporary tree.
+#   We build a static uuid library (libuuid isn't provided on
+#   Android) and link that into libzmq.so. After the build is
+#   done, creates a symlink to the resulting libzmq.so.
+#
+#   The clean.sh script restores the libzmq environment to its
+#   former state.
+
+SRC_DIR="../../src/"
+JNI_DIR="../../src/jni"
+
+NDK_BUILD=`which ndk-build`
+
+if [ -z "$NDK_BUILD" ]; then
+    echo "Must have ndk-build in your path"
+    exit 1
+fi
+
+if [ -z "$LIBZMQ" ]; then
+	echo "You must export variable LIBZMQ to point to zmq library root folder!"
+	exit 2
+fi
+
+if [ -h $LIBZMQ/src/libs/armeabi/libzmq.so ]; then
+	echo "You must build libzmq for android"
+	exit 3
+fi
+
+if [ ! -d $JNI_DIR ]; then
+    mkdir $JNI_DIR
+fi
+
+for i in `ls $SRC_DIR`; do
+    if [ -f $SRC_DIR/$i -a ! -f $JNI_DIR/$i ]; then
+        echo "copying $SRC_DIR/$i --> $JNI_DIR/$i"
+        cp $SRC_DIR/$i $JNI_DIR/$i
+    fi
+done
+
+cp Android.mk $JNI_DIR;
+cp Application.mk $JNI_DIR
+cp $LIBZMQ/src/libs/armeabi/libzmq.so $JNI_DIR/
+
+pushd .
+cd $JNI_DIR
+echo "Current Working dir: `pwd`"
+$NDK_BUILD
+popd
+
+if [ -f ../../src/libs/armeabi/libzmq.so ]; then
+	echo TODO LINK
+fi
+
+
+exit 0
