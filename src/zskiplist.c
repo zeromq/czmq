aeabfff5b0a64d835b3ef630dde688811fb70787
Apostolis Xekoukoulotakis <xekoukou@gmail.com>
2012-11-29 10:03:37 +0200

    license

--- src/zskiplist.c
+++ src/zskiplist.c
@@ -1,3 +1,30 @@
+/*  =========================================================================
+    zsklist - a skip list
+
+    -------------------------------------------------------------------------
+    Copyright (c) 1991-2012 iMatix Corporation <www.imatix.com>
+    Copyright other contributors as noted in the AUTHORS file.
+
+    This file is part of CZMQ, the high-level C binding for 0MQ:
+    http://czmq.zeromq.org.
+
+    This is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This software is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program. If not, see
+    <http://www.gnu.org/licenses/>.
+    =========================================================================
+*/
+
+
 #include "../include/czmq.h"
 
 #define MAX_NODE_HEIGHT 64
