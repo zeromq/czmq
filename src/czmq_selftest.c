1937e53889cab73c79e67b6adae2fdc3ee425e69
Pieter Hintjens <ph@imatix.com>
2013-01-08 14:31:33 +0100

    Updated copyrights to 2013

--- src/czmq_selftest.c
+++ src/czmq_selftest.c
@@ -4,7 +4,7 @@
     Runs all selftests.
 
     -------------------------------------------------------------------------
-    Copyright (c) 1991-2012 iMatix Corporation <www.imatix.com>
+    Copyright (c) 1991-2013 iMatix Corporation <www.imatix.com>
     Copyright other contributors as noted in the AUTHORS file.
 
     This file is part of czmq, the high-level C binding for 0MQ:
--- src/zfile.c
+++ src/zfile.c
@@ -2,7 +2,7 @@
     zfile - helper functions for working with files.
 
     -------------------------------------------------------------------------
-    Copyright (c) 1991-2012 iMatix Corporation <www.imatix.com>
+    Copyright (c) 1991-2013 iMatix Corporation <www.imatix.com>
     Copyright other contributors as noted in the AUTHORS file.
 
     This file is part of CZMQ, the high-level C binding for 0MQ:
