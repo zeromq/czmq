1b374c334e681a2d2037ca78ac58de07c5e89dd6
Pieter Hintjens <ph@imatix.com>
2013-05-31 14:40:10 +0200

    Version number should be 1.4.2

--- include/czmq.h
+++ include/czmq.h
@@ -34,7 +34,7 @@
 
 #define CZMQ_VERSION_MAJOR 1
 #define CZMQ_VERSION_MINOR 4
-#define CZMQ_VERSION_PATCH 0
+#define CZMQ_VERSION_PATCH 2
 
 #define CZMQ_MAKE_VERSION(major, minor, patch) \
     ((major) * 10000 + (minor) * 100 + (patch))
