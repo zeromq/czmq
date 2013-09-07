b6909596623f9fda44a6c4f70faa601d930be7e5
Pieter Hintjens <ph@imatix.com>
2013-09-07 09:33:55 +0200

    Added zsocket_unbind for completeness

--- include/zsocket.h
+++ include/zsocket.h
@@ -9,16 +9,16 @@
     http://czmq.zeromq.org.
 
     This is free software; you can redistribute it and/or modify it under
-    the terms of the GNU Lesser General Public License as published by the 
-    Free Software Foundation; either version 3 of the License, or (at your 
+    the terms of the GNU Lesser General Public License as published by the
+    Free Software Foundation; either version 3 of the License, or (at your
     option) any later version.
 
     This software is distributed in the hope that it will be useful, but
     WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABIL-
-    ITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General 
+    ITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
     Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License 
+    You should have received a copy of the GNU Lesser General Public License
     along with this program. If not, see <http://www.gnu.org/licenses/>.
     =========================================================================
 */
@@ -35,7 +35,7 @@ extern "C" {
 //  We use this when choosing a port for dynamic binding.
 #define ZSOCKET_DYNFROM     0xc000
 #define ZSOCKET_DYNTO       0xffff
-        
+
 //  Callback function for zero-copy methods
 typedef void (zsocket_free_fn) (void *data, void *arg);
 
@@ -58,6 +58,12 @@ CZMQ_EXPORT void
 CZMQ_EXPORT int
     zsocket_bind (void *socket, const char *format, ...);
 
+//  Unbind a socket from a formatted endpoint.
+//  Returns 0 if OK, -1 if the endpoint was invalid or the function
+//  isn't supported.
+CZMQ_EXPORT int
+    zsocket_unbind (void *socket, const char *format, ...);
+
 //  Connect a socket to a formatted endpoint
 //  Returns 0 if OK, -1 if the endpoint was invalid.
 CZMQ_EXPORT int
@@ -73,7 +79,7 @@ CZMQ_EXPORT int
 //  ready on the socket, else FALSE.
 CZMQ_EXPORT bool
     zsocket_poll (void *socket, int msecs);
-    
+
 //  Returns socket type as printable constant string
 CZMQ_EXPORT char *
     zsocket_type_str (void *socket);
@@ -87,11 +93,11 @@ CZMQ_EXPORT int
 //  Returns -1 on error, 0 on success
 //  DEPRECATED - will be removed for next stable release
 CZMQ_EXPORT int
-    zsocket_sendmem_zero_copy (void *socket, void *data, size_t size, 
+    zsocket_sendmem_zero_copy (void *socket, void *data, size_t size,
                                zsocket_free_fn *free_fn,
                                void *hint, int flags);
 
-    
+
 //  Self test of this class
 CZMQ_EXPORT int
     zsocket_test (bool verbose);
