871d8eb216da5da639c1d70c29770accfe6c2a71
Pieter Hintjens <ph@imatix.com>
2013-11-17 15:08:25 +0100

    Allow classes to be private, with header file in src directory

--- builds/android/clean.sh
+++ builds/android/clean.sh
@@ -1,26 +1,8 @@
 #! /bin/bash
-#
-#   Restores the libzmq tree after a build.sh. Does no harm if
-#   used more than once. Run from the builds/android directory.
+#   Restores the tree after a build.sh. Does no harm if used more
+#   than once. Run from the builds/android directory.
+rm -rf ../../src/jni
+rm -rf ../../src/obj
+rm -rf ../../src/libs
+rm -f libczmq.so
 
-
-SRC_DIR="../../src"
-JNI_DIR="$SRC_DIR/jni"
-
-if [ -d $JNI_DIR ]; then
-    rm -rf $JNI_DIR
-fi
-
-if [ -d $SRC_DIR/obj ]; then
-    rm -rf $SRC_DIR/obj
-fi
-
-if [ -d $SRC_DIR/libs ]; then
-    rm -rf $SRC_DIR/libs
-fi
-
-if [ -h libczmq.so ]; then
-    rm libczmq.so
-fi
-
-exit 0
