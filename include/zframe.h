d09374515ec2ca2e5056afb55e668cac6414edfe
Pieter Hintjens <ph@imatix.com>
2014-03-18 15:58:27 +0200

    Removed custom frame API
    - deprecated by zproto, which is better for real protocols

--- include/zframe.h
+++ include/zframe.h
@@ -121,57 +121,6 @@ CZMQ_EXPORT void
 CZMQ_EXPORT int
     zframe_put_block (zframe_t *self, byte *data, size_t size);
 
-//  Put a string to frame payload. Returns 0 if successful else -1.
-//  The string length limited to 2^16 - 1 for '\0' => 65535 characters.
-//  For allocation purpose calculate: (string length + 2) bytes
-CZMQ_EXPORT int
-    zframe_put_string (zframe_t *self, const char *data);
-
-//  Put 1-byte to frame payload. Returns 0 if successful else -1.
-CZMQ_EXPORT int
-    zframe_put_uint8 (zframe_t *self, uint8_t data);
-
-//  Put 2-byte to frame payload. Returns 0 if successful else -1.
-CZMQ_EXPORT int
-    zframe_put_uint16 (zframe_t *self, uint16_t data);
-
-//  Put 4-byte to frame payload. Returns 0 if successful else -1.
-CZMQ_EXPORT int
-    zframe_put_uint32 (zframe_t *self, uint32_t data);
-
-//  Put 8-byte to frame payload. Returns 0 if successful else -1.
-CZMQ_EXPORT int
-    zframe_put_uint64 (zframe_t *self, uint64_t data);
-
-//  Get a block of data from the frame payload.
-CZMQ_EXPORT int
-    zframe_get_block (zframe_t *self, byte *data, size_t size);
-
-//  Get a newly allocated string from frame payload. Returns char pointer to
-//  a string. The max string size can be 2^16 and is 0 terminated.
-CZMQ_EXPORT char *
-    zframe_get_string (zframe_t *self);
-
-//  Get a 1-byte integer from the frame payload by copying it to the 'data'
-//  location. If there was insufficient data in the frame, returns -1.
-CZMQ_EXPORT int
-    zframe_get_uint8 (zframe_t *self, uint8_t *data);
-
-//  Get a 2-byte integer from the frame payload by copying it to the 'data'
-//  location. If there was insufficient data in the frame, returns -1.
-CZMQ_EXPORT int
-    zframe_get_uint16 (zframe_t *self, uint16_t *data);
-
-//  Get a 4-byte integer from the frame payload by copying it to the 'data'
-//  location. If there was insufficient data in the frame, returns -1.
-CZMQ_EXPORT int
-    zframe_get_uint32 (zframe_t *self, uint32_t *data);
-
-//  Get a 8-byte integer from the frame payload by copying it to the 'data'
-//  location. If there was insufficient data in the frame, returns -1.
-CZMQ_EXPORT int
-    zframe_get_uint64 (zframe_t *self, uint64_t *data);
-
 //  Self test of this class
 CZMQ_EXPORT int
     zframe_test (bool verbose);
