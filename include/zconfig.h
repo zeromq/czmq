bba14543e30eb725a0abf111a62915518ae803e9
Stephen Hemminger <stephen@networkplumber.org>
2014-03-18 21:07:07 -0700

    mark functions with printf() semantics with GCC format attribute
    This enables format string checks for CZMQ functions that take
    variable number of arguments in a manner similar to printf().
    
    It also adds a few casts for places where size_t is passed
    to functions with printf like syntax. There is a modifier to allow
    use of size_t (%zd) but it is not portable to all operating systems
    and compilers.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

--- include/zconfig.h
+++ include/zconfig.h
@@ -66,8 +66,9 @@ CZMQ_EXPORT void
 //  comes from an insecure source, you must use '%s' as the format, followed
 //  by the string.
 CZMQ_EXPORT void
-    zconfig_set_value (zconfig_t *self, const char *format, ...);
-
+    zconfig_set_value (zconfig_t *self, const char *format, ...)
+	CZMQ_PRINTF_FUNC(2,3);
+    
 //  Find our first child, if any
 CZMQ_EXPORT zconfig_t *
     zconfig_child (zconfig_t *self);
