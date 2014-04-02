551ac07913e8b7793504a8af1f66e8343eb696fd
Pieter Hintjens <ph@imatix.com>
2014-04-02 10:54:12 +0200

    Added zlog system logging facility
    - on UNIX/LINUX, uses syslog
    - on other systems, does an fprintf() to stderr, for now

--- src/zdigest.c
+++ src/zdigest.c
@@ -2,10 +2,11 @@
     zdigest - provides hashing functions (SHA-1 at present)
 
     -------------------------------------------------------------------------
-    Copyright (c) 1991-2012 iMatix Corporation -- http://www.imatix.com
+    Copyright (c) 1991-2014 iMatix Corporation <www.imatix.com>
     Copyright other contributors as noted in the AUTHORS file.
 
-    This file is part of FILEMQ, see http://filemq.org.
+    This file is part of CZMQ, the high-level C binding for 0MQ:
+    http://czmq.zeromq.org.
 
     This is free software; you can redistribute it and/or modify it under
     the terms of the GNU Lesser General Public License as published by the
@@ -13,12 +14,12 @@
     option) any later version.
 
     This software is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTA-
-    BILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABIL-
+    ITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
     Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with this program. If not, see http://www.gnu.org/licenses/.
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
     =========================================================================
 */
 
