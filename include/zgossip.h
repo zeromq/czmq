3cf6b11507595ae7b1e8482cd9e5e53601ef35a8
Pieter Hintjens <ph@imatix.com>
2014-06-03 16:52:39 +0200

    Added first draft of zgossip class
    - also moved XML models into src so that "make code" works on them
    - zgossip_msg.xml defines messages for gossip protocol
    - zgossip.xml defines the state machine for the gossip server
    - a rough explanation of the gossip model is in zgossip.c
    - next step: a fuller test that explores all edge cases

--- include/zgossip.h
+++ include/zgossip.h
@@ -1,13 +1,23 @@
 /*  =========================================================================
-    zgossip - gossip discovery service
+    zgossip - zgossip server
 
-    Copyright (c) the Contributors as noted in the AUTHORS file.
-    This file is part of CZMQ, the high-level C binding for 0MQ:
-    http://czmq.zeromq.org.
+    ** WARNING *************************************************************
+    THIS SOURCE FILE IS 100% GENERATED. If you edit this file, you will lose
+    your changes at the next build cycle. This is great for temporary printf
+    statements. DO NOT MAKE ANY CHANGES YOU WISH TO KEEP. The correct places
+    for commits are:
 
+    * The XML model used for this code generation: zgossip.xml
+    * The code generation script that built this file: zproto_server_c
+    ************************************************************************
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.       
+    This file is part of CZMQ, the high-level C binding for 0MQ:       
+    http://czmq.zeromq.org.                                            
+                                                                       
     This Source Code Form is subject to the terms of the Mozilla Public
     License, v. 2.0. If a copy of the MPL was not distributed with this
-    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.           
     =========================================================================
 */
 
@@ -19,12 +29,12 @@ extern "C" {
 #endif
 
 //  @interface
-//  This is the gossip service, implemented as a CZMQ zactor task
-CZMQ_EXPORT void
+//  This is zgossip, implemented as a CZMQ zactor task
+void
     zgossip (zsock_t *pipe, void *args);
-    
+
 //  Self test of this class
-CZMQ_EXPORT void
+void
     zgossip_test (bool verbose);
 //  @end
 
