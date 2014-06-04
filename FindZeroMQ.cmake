783fa0904ef7c1e0206d5af6b2423d35b01cca80
Telford Berkey <tberkey@4qbase.com>
2014-06-04 10:31:28 -0400

    Commented out zgossip related items.  Updated FindZeroMQ for windows platforms.

--- FindZeroMQ.cmake
+++ FindZeroMQ.cmake
@@ -1,5 +1,8 @@
 include(FindPkgConfig)
-PKG_CHECK_MODULES(PC_ZEROMQ "libzmq")
+
+if(UNIX)
+    PKG_CHECK_MODULES(PC_ZEROMQ "libzmq")
+endif()
 
 find_path(
     ZEROMQ_INCLUDE_DIRS
@@ -7,11 +10,26 @@ find_path(
     HINTS ${PC_ZEROMQ_INCLUDE_DIRS}
 )
 
+#
+# find release libraries
+#
 find_library(
-    ZEROMQ_LIBRARIES
-    NAMES zmq
+    ZEROMQ_LIBRARIES_RELEASE
+    NAMES libzmq-mt-4_1_0.lib
     HINTS ${PC_ZEROMQ_LIBRARY_DIRS}
 )
+
+#
+# find debug libraries
+#
+find_library(
+    ZEROMQ_LIBRARIES_DEBUG
+    NAMES libzmq-mt-gd-4_1_0.lib
+    HINTS ${PC_ZEROMQ_LIBRARY_DIRS}
+)
+
+set(ZEROMQ_LIBRARIES debug ${ZEROMQ_LIBRARIES_DEBUG}
+                     optimized ${ZEROMQ_LIBRARIES_RELEASE})
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(ZEROMQ DEFAULT_MSG ZEROMQ_LIBRARIES ZEROMQ_INCLUDE_DIRS)
