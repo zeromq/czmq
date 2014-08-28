364cf950f02e2276130c180fdcb5621b3c91c593
Pieter Hintjens <ph@imatix.com>
2014-08-28 16:36:03 +0200

    Problem: 'zinterface' is a dangerous class name
    Under MSVC, which compiles the code as C++, 'interface' is a reserved
    word and this creates regular portability breaks in CZMQ, or forces
    us to use variable names that don't match the class name.
    
    Solution: rename the class to 'ziface'.

--- include/czmq.h
+++ include/czmq.h
@@ -42,7 +42,7 @@ typedef struct _zdir_patch_t zdir_patch_t;
 typedef struct _zfile_t zfile_t;
 typedef struct _zframe_t zframe_t;
 typedef struct _zhash_t zhash_t;
-typedef struct _zinterface_t zinterface_t;
+typedef struct _ziface_t ziface_t;
 typedef struct _zlist_t zlist_t;
 typedef struct _zloop_t zloop_t;
 typedef struct _zmsg_t zmsg_t;
@@ -75,7 +75,7 @@ typedef struct _zproxy_t zproxy_t;
 #include "zframe.h"
 #include "zgossip.h"
 #include "zhash.h"
-#include "zinterface.h"
+#include "ziface.h"
 #include "zlist.h"
 #include "zloop.h"
 #include "zmonitor.h"
