6fcea5375117defca27d2c3c3921d92efdc6d094
Goswin von Brederlow <brederlo@q-leap.de>
2014-09-16 11:48:22 +0200

    zuuid: check for errors in constructor

--- src/zuuid.c
+++ src/zuuid.c
@@ -39,29 +39,31 @@ zuuid_t *
 zuuid_new (void)
 {
     zuuid_t *self = (zuuid_t *) zmalloc (sizeof (zuuid_t));
+    if (self) {
 #if defined (HAVE_LIBUUID)
 #   if defined (__WINDOWS__)
-    UUID uuid;
-    assert (sizeof (uuid) == ZUUID_LEN);
-    UuidCreate (&uuid);
-    zuuid_set (self, (byte *) &uuid);
+        UUID uuid;
+        assert (sizeof (uuid) == ZUUID_LEN);
+        UuidCreate (&uuid);
+        zuuid_set (self, (byte *) &uuid);
 #   else
-    uuid_t uuid;
-    assert (sizeof (uuid) == ZUUID_LEN);
-    uuid_generate (uuid);
-    zuuid_set (self, (byte *) uuid);
+        uuid_t uuid;
+        assert (sizeof (uuid) == ZUUID_LEN);
+        uuid_generate (uuid);
+        zuuid_set (self, (byte *) uuid);
 #   endif
 #else
-    //  No UUID system calls, so generate a random string
-    byte uuid [ZUUID_LEN];
-    int fd = open ("/dev/urandom", O_RDONLY);
-    if (fd != -1) {
-        ssize_t bytes_read = read (fd, uuid, ZUUID_LEN);
-        assert (bytes_read == ZUUID_LEN);
-        close (fd);
-    }
-    zuuid_set (self, uuid);
+        //  No UUID system calls, so generate a random string
+        byte uuid [ZUUID_LEN];
+        int fd = open ("/dev/urandom", O_RDONLY);
+        if (fd != -1) {
+            ssize_t bytes_read = read (fd, uuid, ZUUID_LEN);
+            assert (bytes_read == ZUUID_LEN);
+            close (fd);
+        }
+        zuuid_set (self, uuid);
 #endif
+    }
     return self;
 }
 
