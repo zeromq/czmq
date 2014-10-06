2756ca568ee03c99fcca1a8e73ec46713f7a0acf
Goswin von Brederlow <brederlo@q-leap.de>
2014-10-06 12:57:30 +0000

    Check for pkg-config before using it

--- autogen.sh
+++ autogen.sh
@@ -33,6 +33,12 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+command -v pkg-config >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "autogen.sh: error: could not find pkg-config.  pkg-config is required to run autogen.sh." 1>&2
+    exit 1
+fi
+
 mkdir -p ./config
 if [ $? -ne 0 ]; then
     echo "autogen.sh: error: could not create directory: ./config." 1>&2
