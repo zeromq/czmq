7abf9765d9495f38f33e48b38783d9ac463c818d
Joe McIlvain <joe.eli.mac@gmail.com>
2014-11-05 21:27:06 -0800

    Problem: Build system for qt-android is out of date. Solution: Update it.

--- builds/qt-android/ci_build.sh
+++ builds/qt-android/ci_build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Assumes Travis CI os:linux (x86_64)
 (cd '/tmp' \
     && wget http://dl.google.com/android/ndk/android-ndk-r9-linux-x86_64.tar.bz2 \
     && tar -xf android-ndk-r9-linux-x86_64.tar.bz2 \
@@ -11,7 +12,7 @@ export TOOLCHAIN_NAME="arm-linux-androideabi-4.8"
 export TOOLCHAIN_HOST="arm-linux-androideabi"
 export TOOLCHAIN_ARCH="arm"
 
-export LIBZMQ_ROOT="/tmp/libzmq"
-git clone git://github.com/zeromq/libzmq.git $LIBZMQ_ROOT
+export ZMQ_ROOT="/tmp/libzmq"
+git clone git://github.com/zeromq/libzmq.git $ZMQ_ROOT
 
 source ./build.sh
