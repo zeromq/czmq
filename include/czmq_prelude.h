7b6c3804dbed0d77455cef4edb0e789d78e5dd85
Gran Ville Lintao <granvillelintao@gmail.com>
2014-12-09 18:08:12 +0400

    Ref #855

--- include/czmq_prelude.h
+++ include/czmq_prelude.h
@@ -434,8 +434,12 @@ typedef struct sockaddr_in inaddr_t;    //  Internet socket address structure
 #       endif
 #   endif
 #   if (!defined (__MINGW32__) || (defined (__MINGW32__) && defined (__IS_64BIT__)))
+    typedef __int8 int8_t;
+    typedef __int16 int16_t;
     typedef __int32 int32_t;
     typedef __int64 int64_t;
+    typedef unsigned __int8 uint8_t;    
+    typedef unsigned __int16 uint16_t;
     typedef unsigned __int32 uint32_t;
     typedef unsigned __int64 uint64_t;
 #   endif    
