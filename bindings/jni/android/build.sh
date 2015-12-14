31be1e6ce5f5ef1663df0ae09b63cedc52e2f698
Pieter Hintjens <ph@imatix.com>
2015-12-14 11:20:50 +0100

    Problem: need access to Android environment vars
    Solution: copy android_build_helper.sh from qt-android.
    
    This is a hack and will be replaced by a backend in zproject,
    when it is working.

--- bindings/jni/android/build.sh
+++ bindings/jni/android/build.sh
@@ -1,9 +1,12 @@
-# copy me in czmq/bindings/jni/buidlme.sh and make me executable with chmod +x
 #!/bin/bash
+# copy me in czmq/bindings/jni/buidlme.sh and make me executable with chmod +x
+source android_build_helper.sh
+android_build_env
+
 rm -Rf build
 mkdir build
 cd build
 cmake -v -DCMAKE_TOOLCHAIN_FILE=../android_toolchain.cmake ..
 ln -s $ANDROID_NDK_ROOT/platforms/android-8/arch-arm/usr/lib/crtend_so.o
 ln -s $ANDROID_NDK_ROOT/platforms/android-8/arch-arm/usr/lib/crtbegin_so.o
-make VERBOSE=1
+make
