96115f000bc8e6ed50c1d495b4c177b42157ea84
Pieter Hintjens <ph@imatix.com>
2015-12-15 10:35:35 +0100

    Problem: Android build depends on JNI build
    Solution: in jni/android/build.sh, always call gradle build for
    JNI.
    
    Also, collect outputs of Android build, JNI build, and JNI/Android
    build in a single directory, build/lib.

--- bindings/jni/android/build.sh
+++ bindings/jni/android/build.sh
@@ -3,12 +3,33 @@
 source ../../../builds/android/android_build_helper.sh
 android_build_env
 
+#   Ensure we've built dependencies for Android
+echo "********  Building Android native libraries"
 ( cd ../../../builds/android && ./build.sh )
+
+#   Ensure we've built JNI interface
+echo "********  Building JNI interface & classes"
+( cd .. && gradle build jar )
 
 rm -Rf build
 mkdir build
 cd build
 cmake -v -DCMAKE_TOOLCHAIN_FILE=../android_toolchain.cmake ..
+
+#   CMake wrongly searches current directory and then toolchain path instead
+#   of lib path for these files, so make them available temporarily
 ln -s $ANDROID_NDK_ROOT/platforms/android-8/arch-arm/usr/lib/crtend_so.o
 ln -s $ANDROID_NDK_ROOT/platforms/android-8/arch-arm/usr/lib/crtbegin_so.o
+
+echo "********  Building JNI for Android"
 make #VERBOSE=1
+rm crtend_so.o crtbegin_so.o
+cd ..
+
+mkdir build/libs
+mv build/*.so build/libs
+cp ../build/libs/* build/libs
+cp ../../../builds/android/prefix/*/lib/*.so build/libs
+
+echo "********  Complete: build products are in ./build/libs"
+ls build/libs
