2d380ae4d083fb448101d3935c8f0117c002e596
Pieter Hintjens <ph@imatix.com>
2016-04-09 15:33:20 +0200

    Problem: NodeJS binding can't do Hello World
    Solution:
    
     - add sockish type to API models
     - add support for this in zproject nodejs target
     - add test_binding.js example for Hello, World

---
+++ bindings/nodejs/test_binding.js
@@ -0,0 +1,33 @@
+/*  =========================================================================
+    test_binding.js - hand-written test cases for NodeJS binding
+
+    -------------------------------------------------------------------------
+    Copyright (c) the Contributors as noted in the AUTHORS file.
+    This file is part of CZMQ, the high-level C binding for 0MQ:
+    http://czmq.zeromq.org.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+    =========================================================================
+*/
+
+var czmq = require ('bindings')('czmq')
+var zstr = new czmq.Zstr ();
+
+var server = new czmq.Zsock (12)
+server.bind ("inproc://test-endpoint")
+var client = new czmq.Zsock (13)
+client.connect ("inproc://test-endpoint")
+
+zstr.send (client, "Hello")
+var request = zstr.recv (server)
+console.log (request)
+
+zstr.send (server, "World")
+var reply = zstr.recv (client)
+console.log (reply)
+
+server.destroy ()
+client.destroy ()
+
