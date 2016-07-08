a49789e3e14ffb0d029722a3f63407aabb7b08de
Kevin Sapper <mail@kevinsapper.de>
2016-07-08 08:09:57 +0000

    Problem: czmq is out of sync (zproject has evolved!)
    Solution: re-generate all!

--- builds/cmake/ci_build.sh
+++ builds/cmake/ci_build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-set -x
+set -ex
 
 mkdir tmp
 BUILD_PREFIX=$PWD/tmp
@@ -11,6 +10,9 @@ CONFIG_OPTS+=("CPPFLAGS=-I${BUILD_PREFIX}/include")
 CONFIG_OPTS+=("CXXFLAGS=-I${BUILD_PREFIX}/include")
 CONFIG_OPTS+=("LDFLAGS=-L${BUILD_PREFIX}/lib")
 CONFIG_OPTS+=("PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig")
+CONFIG_OPTS+=("--prefix=${BUILD_PREFIX}")
+CONFIG_OPTS+=("--with-docs=no")
+CONFIG_OPTS+=("--quiet")
 
 CMAKE_OPTS=()
 CMAKE_OPTS+=("-DCMAKE_INSTALL_PREFIX:PATH=${BUILD_PREFIX}")
@@ -18,9 +20,24 @@ CMAKE_OPTS+=("-DCMAKE_PREFIX_PATH:PATH=${BUILD_PREFIX}")
 CMAKE_OPTS+=("-DCMAKE_LIBRARY_PATH:PATH=${BUILD_PREFIX}/lib")
 CMAKE_OPTS+=("-DCMAKE_INCLUDE_PATH:PATH=${BUILD_PREFIX}/include")
 
-# Build, check, and install ZeroMQ
-git clone git://github.com/zeromq/libzmq.git &&
-( cd libzmq; ./autogen.sh && ./configure "${CONFIG_OPTS[@]}" --prefix=${BUILD_PREFIX} && make check && make install ) || exit 1
+# Clone and build dependencies
+git clone --quiet --depth 1 https://github.com/zeromq/libzmq libzmq
+cd libzmq
+git --no-pager log --oneline -n1
+if [ -e autogen.sh ]; then
+    ./autogen.sh 2> /dev/null
+fi
+if [ -e buildconf ]; then
+    ./buildconf 2> /dev/null
+fi
+./configure "${CONFIG_OPTS[@]}"
+make -j4
+make install
+cd ..
 
-# Build, check, and install CZMQ from local source
-( cd ../..; mkdir build_cmake && cd build_cmake && PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig cmake "${CMAKE_OPTS[@]}" .. && make all VERBOSE=1 && make install ) || exit 1
+# Build and check this project
+cd ../..
+PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig cmake "${CMAKE_OPTS[@]}" .
+make all VERBOSE=1 -j4
+ctest -V
+make install
