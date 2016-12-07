70d59ff7dc1331fbc367f78e1c2adca41370e547
Luca Boccassi <luca.boccassi@gmail.com>
2016-12-07 13:46:21 +0000

    Problem: older ZMQ does not know --with-docs
    Solution: use --without-documentation as a fallback in the legacy
    CI builds

--- builds/stable_zmq/ci_build.sh
+++ builds/stable_zmq/ci_build.sh
@@ -14,6 +14,7 @@ CONFIG_OPTS+=("LDFLAGS=-L${BUILD_PREFIX}/lib")
 CONFIG_OPTS+=("PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig")
 CONFIG_OPTS+=("--prefix=${BUILD_PREFIX}")
 CONFIG_OPTS+=("--with-docs=no")
+CONFIG_OPTS+=("--without-documentation")
 CONFIG_OPTS+=("--quiet")
 
 git clone --quiet --depth 1 -b stable git://github.com/jedisct1/libsodium.git &&
