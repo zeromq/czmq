4057b44cf405c3470e4bdb64d1523c0103c694c2
Luca Boccassi <luca.boccassi@gmail.com>
2017-05-10 16:45:26 +0100

    Problem: zmsg_recv_nowait doesn't complete multipart receives
    Solution: implement same check as in zmsg_recv, and if a frame has
    already been received and the error is an interrupt try again

--- src/zmsg.c
+++ src/zmsg.c
@@ -858,8 +858,12 @@ zmsg_recv_nowait (void *source)
     while (true) {
         zframe_t *frame = zframe_recv_nowait (source);
         if (!frame) {
-            zmsg_destroy (&self);
-            break;              //  Interrupted or terminated
+            if (errno == EINTR && zlist_head (self->frames))
+                continue;
+            else {
+                zmsg_destroy (&self);
+                break;              //  Interrupted or terminated
+            }
         }
         if (zmsg_append (self, &frame)) {
             zmsg_destroy (&self);
