d05bfc2d5a9bdab0b87277d6e6411d85340a4997
Kenji Rikitake <kenji.rikitake@acm.org>
2018-01-03 22:31:49 +0900

    Fix zuuid.c build error on FreeBSD on v4.1.0 (and master)
    * This fix works on FreeBSD 10.4-RELEASE and 11.1-RELEASE, 11.1-STABLE
    * Forcefully define HAVE_UUID in czmq_prelude.h if FreeBSD
        - FreeBSD has the stock UUID library, but it's not -luuid, it's in -lc, so HAVE_UUID is not defined during configure
    * Skip code in zuuid.c assuming uuid_generate(2) if FreeBSD
        - FreeBSD does not have uuid_generate(2) (it's Linux specific)
    
    See https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=224853 for the details

--- include/czmq_prelude.h
+++ include/czmq_prelude.h
@@ -683,6 +683,9 @@ char *if_indextoname (unsigned int ifindex, char *ifname);
 #if defined (__UTYPE_OSX) && !defined (HAVE_UUID)
 #   define HAVE_UUID 1
 #endif
+#if defined (__UTYPE_FREEBSD) && !defined (HAVE_UUID)
+#   define HAVE_UUID 1
+#endif
 #if defined (HAVE_UUID)
 #   if defined (__UTYPE_FREEBSD) || defined (__UTYPE_NETBSD) || defined(__UTYPE_OPENBSD)
 #       include <uuid.h>
