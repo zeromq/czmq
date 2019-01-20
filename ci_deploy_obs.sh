0a0086596fc071eda4624ab5b5bcd0ac12419032
Luca Boccassi <bluca@debian.org>
2019-01-20 17:10:29 +0000

    Problem: no ABI checker, OBS release broken
    Solution: regenerate from zproject

--- ci_deploy_obs.sh
+++ ci_deploy_obs.sh
@@ -14,10 +14,9 @@ if [ "$BUILD_TYPE" == "default" -a -n "${GH_TOKEN}" -a -n "${OBS_STABLE_TOKEN}" 
     # not possible to edit files on OBS with secure tokens, and it is not
     # possible to dynamically fetch the latest git tag either.
     TAG_SHA=$(curl -s -H "Authorization: token ${GH_TOKEN}" -X GET https://api.github.com/repos/zeromq/czmq/git/refs/tags/${TRAVIS_TAG} | grep -o -P '(?<=sha":\s).*(?=,)')
-    curl -H "Authorization: token ${GH_TOKEN}" -X POST --data "{\"ref\":\"refs/heads/tmp_obs_release_branch\",\"sha\":${TAG_SHA}}" https://api.github.com/repos/zeromq/czmq/git/refs
+    curl -H "Authorization: token ${GH_TOKEN}" -X DELETE https://api.github.com/repos/zeromq/czmq/git/refs/heads/latest_release
+    curl -H "Authorization: token ${GH_TOKEN}" -X POST --data "{\"ref\":\"refs/heads/latest_release\",\"sha\":${TAG_SHA}}" https://api.github.com/repos/zeromq/czmq/git/refs
+    sleep 2 # try to avoid races if Github is slow
     curl -H "Authorization: Token ${OBS_STABLE_TOKEN}" -X POST https://api.opensuse.org/trigger/runservice
     curl -H "Authorization: Token ${OBS_DRAFT_TOKEN}" -X POST https://api.opensuse.org/trigger/runservice
-    # give some time for the git clone to happen before deleting the temp branch
-    sleep 60
-    curl -H "Authorization: token ${GH_TOKEN}" -X DELETE https://api.github.com/repos/zeromq/czmq/git/refs/heads/tmp_obs_release_branch
 fi
