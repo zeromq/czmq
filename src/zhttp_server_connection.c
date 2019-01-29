d57569a829d335e55b3c03aee90a4345eef68ea2
somdoron <somdoron@gmail.com>
2019-01-29 13:35:25 +0200

    Problem: CZMQ doesn't have an http server
    Using http server libraries together with czmq is hard.
    You have to integrate czmq event loop with the the library event loop, which is not an easy task.
    
    Solution: Wrap an existing library and provide CZMQ style api.

---
+++ src/zhttp_server_connection.c
@@ -0,0 +1,24 @@
+/*  =========================================================================
+    zhttp_connection - class description
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+    =========================================================================
+*/
+
+/*
+@header
+    zhttp_server_connection -
+@discuss
+@end
+*/
+
+#include "czmq_classes.h"
+
+void
+zhttp_server_connection_test (bool verbose) {
+
+}
