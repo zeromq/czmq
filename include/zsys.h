1d16ed1aefd51e777b57f9572f4d092f0a2bf519
Jim Klimov <jim@jimklimov.com>
2019-01-31 16:21:48 +0100

    Problem: zsys_sprintf work twice for large texts
    Solution: add a version of the routine that allows caller to hint
    that this invocation will require a larger buffer to process text
    just once and succeed.
    
    Signed-off-by: Jim Klimov <EvgenyKlimov@eaton.com>

--- include/zsys.h
+++ include/zsys.h
@@ -141,6 +141,14 @@ CZMQ_EXPORT void
 
 //  Format a string using printf formatting, returning a freshly allocated
 //  buffer. If there was insufficient memory, returns NULL. Free the returned
+//  string using zstr_free(). The hinted version allows to optimize by using
+//  a larger starting buffer size (known to/assumed by the developer) and so
+//  avoid reallocations.
+CZMQ_EXPORT char *
+    zsys_sprintf_hint (int hint, const char *format, ...);
+
+//  Format a string using printf formatting, returning a freshly allocated
+//  buffer. If there was insufficient memory, returns NULL. Free the returned
 //  string using zstr_free().
 CZMQ_EXPORT char *
     zsys_sprintf (const char *format, ...);
