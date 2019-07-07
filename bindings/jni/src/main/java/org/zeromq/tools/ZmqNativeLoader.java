1c1507dbf18af6d880cd5395f294a8d952173f0b
Kevin Sapper <mail@kevinsapper.de>
2019-07-07 20:59:31 +0200

    Problem: Out of sync with zproject
    Solution: Re-generate!

--- bindings/jni/src/main/java/org/zeromq/tools/ZmqNativeLoader.java
+++ bindings/jni/src/main/java/org/zeromq/tools/ZmqNativeLoader.java
@@ -10,14 +10,14 @@ public class ZmqNativeLoader {
 
     public static void loadLibrary(String libname) {
         if (!loadedLibraries.contains(libname)) {
-            if (System.getProperty("java.vm.vendor").contains("Android")) {
-                System.loadLibrary(libname);
-            } else {
-                try {
+            try {
+                if (System.getProperty("java.vm.vendor").contains("Android")) {
+                    System.loadLibrary(libname);
+                } else {
                     NativeLoader.loadLibrary(libname);
-                } catch (Exception e) {
-                    System.err.println("[WARN] " + e.getMessage() +" from jar. Assuming it is installed on the system.");
                 }
+            } catch (Exception e) {
+                System.err.println("[WARN] " + e.getMessage() +" from jar. Assuming it is installed on the system.");
             }
             loadedLibraries.add(libname);
         }
