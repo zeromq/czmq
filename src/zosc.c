7d8421ac6d88b6b2d1c41d79ac84bf7688b88509
Arnaud Loonstra <arnaud@sphaero.org>
2021-05-05 19:21:53 +0200

    fix for cross compilation on bsd and mingw platforms

--- src/zosc.c
+++ src/zosc.c
@@ -18,11 +18,17 @@
 @end
 */
 
-#ifdef WIN32
-#include <winsock2.h>           //  needed for ntohll/htonll
-#endif
 #include "czmq_classes.h"
-#if __unix__ && !__APPLE__
+#if defined( __WINDOWS__ )
+#include <winsock2.h>           //  needed for ntohll/htonll
+#elif defined(__UTYPE_FREEBSD) || defined(__UTYPE_NETBSD)
+#include <sys/endian.h>
+#define htonll(x) htobe64(x)
+#define ntohll(x) be64toh(x)
+#elif defined(__MINGW32__)
+#define htonll(x) ((((uint64_t)htonl(x&0xFFFFFFFF)) << 32) + htonl(x >> 32))
+#define ntohll(x) ((((uint64_t)ntohl(x&0xFFFFFFFF)) << 32) + ntohl(x >> 32))
+#elif defined(__UNIX__)
 #include <endian.h>
 #define htonll(x) htobe64(x)
 #define ntohll(x) be64toh(x)
